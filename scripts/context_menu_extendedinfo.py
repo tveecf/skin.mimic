d8cd6f170e271dfdf30eeb51888a3a92a7056b64
Randall <randallspicher@gmail.com>
2020-08-29 19:36:06 -0400

    clean up alternate-icon logic

--- scripts/context_menu_extendedinfo.py
+++ scripts/context_menu_extendedinfo.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from future import standard_library
+standard_library.install_aliases()
+
 #
 # Copyright (C) 2015 Philipp Temminghoff
 #
@@ -19,27 +24,30 @@ import xbmc
 import xbmcgui
 import sys
 
-
 def main():
-    info = sys.listitem.getVideoInfoTag()
-    dbid = info.getDbId()
-    db_type = info.getMediaType()
-    remote_id = sys.listitem.getProperty("id")
-    BASE = "RunScript(script.extendedinfo,info="
-    if not dbid:
-        dbid = sys.listitem.getProperty("dbid")
-    if db_type == "movie":
-        xbmc.executebuiltin("%sextendedinfo,dbid=%s,id=%s,name=%s)" % (BASE, dbid, remote_id, info.getTitle()))
-    elif db_type == "tvshow":
-        xbmc.executebuiltin("%sextendedtvinfo,dbid=%s,id=%s)" % (BASE, dbid, remote_id))
-    elif db_type == "season":
-        xbmc.executebuiltin("%sseasoninfo,tvshow=%s,season=%s)" % (BASE, info.getTVShowTitle(), info.getSeason()))
-    elif db_type in ["actor", "director"]:
-        xbmc.executebuiltin("%sextendedactorinfo,name=%s)" % (BASE, sys.listitem.getLabel()))
+	
+	info = sys.listitem.getVideoInfoTag()
+	dbid = info.getDbId()
+	db_type = info.getMediaType()
+	remote_id = sys.listitem.getProperty("id")
+	BASE = "RunScript(script.extendedinfo,info="
+	if not dbid:
+		dbid = sys.listitem.getProperty("dbid")
+	if db_type == "movie":
+		xbmc.executebuiltin("%sextendedinfo,dbid=%s,id=%s,name=%s)" % (BASE, dbid, remote_id, info.getTitle()))
+	elif db_type == "tvshow":
+		xbmc.executebuiltin("%sextendedtvinfo,dbid=%s,id=%s)" % (BASE, dbid, remote_id))
+	elif db_type == "season":
+		xbmc.executebuiltin("%sseasoninfo,tvshow=%s,season=%s)" % (BASE, info.getTVShowTitle(), info.getSeason()))
+	elif db_type in ["actor", "director"]:
+		xbmc.executebuiltin("%sextendedactorinfo,name=%s)" % (BASE, sys.listitem.getLabel()))
 
 home = xbmcgui.Window(10000)
 skin=home.getProperty("CurrentSkin")
 if skin == "skin.lyrebird.rmod":
-    main()
+	main()
 else:
-    pass     
+	pass	 
+
+
+
